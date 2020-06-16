27000aa16863673824c3f83bef089047a8592a33
Leon <leonbaraza@gmail.com>
2020-06-16 12:16:45 +0300

    Initial Commit - Routes and Public folder

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+// Creating a middleware
+app.use(express.static(path.join(__dirname,'public')))
+
+// Sending a file
+app.get('/',  (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>console.log(`Server started on post ${PORT}`));
