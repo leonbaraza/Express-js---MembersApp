7856749ce51d85c7e95c92103443987399514918
Leon <leonbaraza@gmail.com>
2020-06-16 12:27:42 +0300

    Get all members Json

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ const path = require('path')
 
 const app = express();
 
+const members = [
+    {
+        id:1,
+        name : 'Leon Wakoli',
+        email : 'leonbaraza@gmail.com',
+        status : 'active'
+    },
+    {
+        id : 2,
+        name : 'Salome Wamachaba',
+        email : 'wamachaba@gmail.com',
+        status : 'inactive'
+    },
+    {
+        id : 3,
+        name : 'Patrick Wafula',
+        email : 'pat@gmail.com',
+        status : 'active'
+    }
+]
+
+// Gets all members
+app.get('/api/members', (req, res) => res.json(members));
+
 // Creating a middleware
 app.use(express.static(path.join(__dirname,'public')))
 
 // Sending a file
-app.get('/',  (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/',  (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 3000;
 
